a102aff88c3dbf5672909f411f74e3cc25e9b8c7
kuharan <agamoni@thirdeyedata.io>
2020-03-25 20:41:15 +0530

    Code Deployment

--- ContosoComicPowerBIEmbed/App_Start/FilterConfig.cs
+++ ContosoComicPowerBIEmbed/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace ContosoComicPowerBIEmbed
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new ErrorHandler.AiHandleErrorAttribute());
         }
     }
 }
